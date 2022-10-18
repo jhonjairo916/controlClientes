df0586e3596201c4abf0c9a94d8762b690ffd206
Jhon Jairo <jhonjairo.916@hotmail.com>
2022-10-18 15:21:41 -0500

    the navbar is ended

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:TableroComponent},
+  {path:'login',component:LoginComponent},
+  {path:'registrarse',component:RegistroComponent},
+  {path:'configuracion',component:ConfiguracionComponent},
+  {path:'clientes/',component:ClientesComponent},
+  {path:'clientes/editar/:id',component:EditarClienteComponent},
+  {path:'**',component:NoEncontradoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    ClientesComponent,
+    ConfiguracionComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent,
+    RegistroComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
